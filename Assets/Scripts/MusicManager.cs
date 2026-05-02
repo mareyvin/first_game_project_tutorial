6b668eeb4ae44b67b6b8aff2b2136f953d3661c8
mareyvin <teaofch12@gmail.com>
2026-05-02 04:30:56 +0300

    added music + assets + collider

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+    private static MusicManager instance;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+
+    }
+}
